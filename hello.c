6538ac3c72d1e98926a3ac207465e11d17bceb10
lukereynoldspi <lukereynoldspi@gmail.com>
2022-01-12 18:37:27 -0800

    started on hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int x = 12;
+	float y = 3.14;
+	char s[] = "Hello, world!";
+	printf("x is %d, y is %.6f", x, y);
+	printf("%s", s);
+
+	int i;
+	for (i = 1; i < 5; ++i)
+	{
+		
+		printf("%d ", i);
+	}
+}
