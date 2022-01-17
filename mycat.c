f5975b95bbe177dd251240d8ad836e29dd7cc071
lukereynoldspi <lukereynoldspi@gmail.com>
2022-01-16 16:14:21 -0800

    finished mycat.c

--- mycat.c
+++ mycat.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-
 int main(int argc, char *argv[]) {
 
 	int fd = open(argv[1], O_RDONLY);
-
 	if (fd == -1 && argc <2) {
     	perror("open");
 	}
-	char buf[2048];
-	int count = read(fd, buf, sizeof buf);
-
-	write(STDOUT_FILENO, buf, count);
-	close(fd);
+	int i = 1;
+	
+	while ((fd != 0) || (fd != -1)) {
+		int fd = open(argv[i], O_RDONLY);
+		char buf[2048];
+		int count = read(fd, buf, sizeof buf);
+		write(STDOUT_FILENO, buf, count);
+		close(fd);
+		i++;
+	}
 }
